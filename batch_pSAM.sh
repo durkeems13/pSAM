be4eb54c5d64dfd5fc6c0a6e95a96c64bf5a9de8
durkeems13 <67382175+durkeems13@users.noreply.github.com>
2025-02-09 15:50:19 -0600

    Update batch_pSAM.sh

--- batch_pSAM.sh
+++ batch_pSAM.sh
@@ -7,15 +7,28 @@ echo "Running pSAM. You can check output while running and stop if progress is n
 
 echo ""
 echo "Calculating class maps..."
-#python3 spectral_angle_mapping-newCSV.py --dataset $DATASET --sample $SAMPLE --area $AREA 
+python3 spectral_angle_mapping.py --dataset $DATASET --sample $SAMPLE --area $AREA 
 echo ""
 echo "Class maps calculated."
 
 echo ""
 echo "Running pixel SAM analysis..."
-#python3 pixel_class_analysis.py --dataset $DATASET --sample $SAMPLE --area $AREA
+python3 pixel_class_analysis.py --dataset $DATASET --sample $SAMPLE --area $AREA
 echo ""
 echo "MIPs and plots saved."
+
+echo ""
+echo "Collecting cell segmentations..."
+source activate cellpose
+cd ../cell_instance_segmentation/CellPose
+python3 cellpose_wholeSlide.py --dataset $DATASET --sample $SAMPLE --area $AREA
+cd ../nuclear_dilation
+conda deactivate
+python3 dilate_nuclear_segmentations-wholeSlide.py --dataset $DATASET --sample $SAMPLE --area $AREA
+cd ../../spectral_angle_mapping
+echo ""
+echo "Cell segmentations done."
+
 
 echo ""
 echo "Assigning pSAM class to segmented nuclei..."
